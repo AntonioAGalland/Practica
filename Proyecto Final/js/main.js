0dbc13076884d05d48476e3de6be8e52964517cf
Antonio Galland <antonio.galland@gmail.com>
2023-03-01 00:31:34 -0300

    Proyecto final con busqueda funcionando

--- Proyecto Final/js/main.js
+++ Proyecto Final/js/main.js
@@ -7,4 +7,28 @@ const grid = new Muuri('.grid',  {
 window.addEventListener('load', () => {
     grid.refreshItems().layout();       
     document.getElementById('grid').classList.add('imagenes-cargadas');
-});
+
+    //agregamos los listener de los enlaces para filtrar por categoría
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            console.log(evento.target);
+            enlaces.forEach((enlace)=>enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+            const categoria = evento.target.innerHTML.toLowerCase();
+            console.log(categoria);
+            categoria === 'todos' ? 
+                grid.filter(`[data-categoria]`) : 
+                grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+
+    // agregamos los listener para la barra de búsqueda
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) ); 
+    });
+}); 
